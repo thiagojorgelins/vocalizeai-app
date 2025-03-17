3ef71d5b0cc02f4a8c1bbc68ce1fb8b122f33b47
Akkadius <thiago.jorge777@gmail.com>
2025-03-17 19:39:57 -0300

    feat: Add componente para o select das vocalizações

---
+++ components/VocalizationSelect.tsx
@@ -0,0 +1,34 @@
+import Select from "@/components/Select";
+import translateVocalization from "@/utils/TranslateVocalization";
+import React from "react";
+import { VocalizationSelectProps } from "../types/VocalizationSelectProps";
+
+
+
+export default function VocalizationSelect ({
+  vocalizations,
+  selectedVocalizationId,
+  onValueChange,
+  label = "Tipo de Vocalização",
+}: VocalizationSelectProps ) {
+  const sortedOptions = vocalizations
+    .map((voc) => ({
+      id: voc.id,
+      value: voc.id.toString(),
+      label: translateVocalization[voc.nome] || voc.nome,
+      original: voc.nome,
+    }))
+    .sort((a, b) => a.label.localeCompare(b.label, "pt-BR"));
+
+  return (
+    <Select
+      label={label}
+      selectedValue={selectedVocalizationId?.toString() || ""}
+      onValueChange={(itemValue) => onValueChange(Number(itemValue))}
+      options={sortedOptions.map((option) => ({
+        label: option.label,
+        value: option.value,
+      }))}
+    />
+  );
+};
